c32b9ca737f2d6f6252ac07569dea879ffe11bd5
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-09 15:39:39 -0500

    wrote tests for name input, checking toppings, and submitting. all tests passed

--- cypress/e2e/pizza.cy.js
+++ cypress/e2e/pizza.cy.js
@@ -1,17 +1,41 @@
 describe('BloomTech Eats App', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:3000');
+        cy.visit('http://localhost:3000/pizza');
     })
 
-    const nameInput = () => cy.get('');
-    const toppingSelect = () => cy.get('');
-    const submitBtn = () => cy.get(``);
+    const nameInput = () => cy.get('input[name=name]');
+    const baconCheckbox = () => cy.get('input[name=bacon]');
+    const mushroomsCheckbox = () => cy.get('input[name=mushrooms]');
+    const submitBtn = () => cy.get(`button[id='order-button']`);
 
     it('the proper elements are showing', () => {
-        
+        nameInput().should('exist');
+        baconCheckbox().should('exist');
+        mushroomsCheckbox().should('exist');
+        submitBtn().should('exist');
     })
 
+    describe('Filling out inputs', () => {
+        it('can name in input', () => {
+            nameInput()
+                .should('have.value', '')
+                .type('Vanessa')
+                .should('have.value', 'Vanessa');
+        })
+    })
 
+    describe('Checking boxes', () => {
+        it('can check pizza toppings', () => {
+            baconCheckbox().check();
+            mushroomsCheckbox().check();
+        })
+    })
+
+    describe('Submit the form', () => {
+        it('can submit the pizza order form', () => {
+            submitBtn().click();
+        })
+    })
 
 
 
