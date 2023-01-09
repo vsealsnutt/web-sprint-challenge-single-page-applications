4662c58b50fbcc40fddfbf2ba1f82238f49386c6
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-09 08:53:14 -0500

    installed dependencies and started building nav links in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <h1>BloomTech Eats</h1>
+      <nav>
+        <div className="nav-links">
+          <Link to='/'>Home</Link>
+          <Link to='/help'>Help</Link>
+        </div>
+      </nav>
+    </div>
   );
 };
 export default App;
