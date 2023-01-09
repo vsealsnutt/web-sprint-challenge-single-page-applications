d7228969af42c149e62e456c6b38796f778f6e31
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-01-09 09:37:51 -0500

    started building out the confirmation page

--- src/Components/ConfirmationPage.js
+++ src/Components/ConfirmationPage.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Confirmation = () => {
-
+    return (
+        <div className='confirmation-wrapper'>
+            <div className='top'>
+                <h2>Congrats! Pizza is on its way!</h2>
+            </div>
+            <div className='bottom'>
+                <h3>Enjoy this Dog with Pizza</h3>
+                <img src='https://tenor.com/rjR3.gif' alt='Puppy on a chair with a pizza slice in its mouth'></img>
+            </div>
+        </div>
+    )
 }
 
 export default Confirmation;
